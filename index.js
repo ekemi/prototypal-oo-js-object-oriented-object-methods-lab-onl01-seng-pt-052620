c5b4df651526b0e9397053e637d6fa535c62e4c3
edmundo papi <edmundopierre@yahoo.com>
2021-02-27 22:55:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function BoardMember (name, homeState,training) {
   this.name =name;
   this.homeState = homeState;
   this.training = training;
-  this.ve = function (){
+  this.veto = function (){
     return "No, I must disagree";
   }
 
