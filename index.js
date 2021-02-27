5942c1b0c56d2fc1e3ddacc6f647da044c054345
edmundo papi <edmundopierre@yahoo.com>
2021-02-27 23:08:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function BoardMember (name, homeState,training) {
   }
 
   this.sayHi = function(){
-     return "Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.";
+     return "Hi, my name is <name>. I am from `${this.homestate}`, and I was trained in `${this.training}`.";
   }
 
 }
